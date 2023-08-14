690c401bd52ebad23ba0f759a379b8c6b88823cd
killReal99 <85346432+killReal99@users.noreply.github.com>
2023-08-14 12:12:44 +0300

    Update boot parameters setting

--- kernel_script.sh
+++ kernel_script.sh
@@ -26,10 +26,10 @@ nice make -j8 bindeb-pkg
 
 update-initramfs -c -k 5.10.180-rt89
 
-ln -s /boot/initrd.img-5.10.180-rt89-rc1 /boot/initrd.img
-ln -s /boot/config-5.10.180-rt89-rc1 /boot/config
-ln -s /boot/System.map-5.10.180-rt89-rc1 /boot/System.map
-ln -s /boot/vmlinuz-5.10.180-rt89-rc1 /boot/vmlinuz
+ln -s /boot/initrd.img-5.10.187-rt91 /boot/initrd.img
+ln -s /boot/config-5.10.187-rt91 /boot/config
+ln -s /boot/System.map-5.10.187-rt91 /boot/System.map
+ln -s /boot/vmlinuz-5.10.187-rt91 /boot/vmlinuz
 
 update-grub
 
