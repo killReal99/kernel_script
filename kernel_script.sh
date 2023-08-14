6879fe1b62c2dc89dbbf7f999d49e3fdea059fda
killReal99 <85346432+killReal99@users.noreply.github.com>
2023-08-14 14:55:08 +0300

    Fix kernel version

--- kernel_script.sh
+++ kernel_script.sh
@@ -9,12 +9,12 @@ kconfig-hardened-check -c /root/config-5.10.180-rt89-amd64 -l /proc/cmdline -m s
 
 git config --global http.sslVerify "false"
 
-wget --no-check-certificate  https://git.linuxtesting.ru/pub/scm/linux/kernel/git/lvc/linux-stable.git/snapshot/linux-stable-5.10.187-lvc10.tar.xz
+wget --no-check-certificate  https://git.linuxtesting.ru/pub/scm/linux/kernel/git/lvc/linux-stable.git/snapshot/linux-stable-5.10.186-lvc9.tar.xz
 wget https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.10/patch-5.10.186-rt91.patch.gz
 
 cd /usr/src/
-tar xf ~/linux-stable-5.10.187-lvc10.tar.xz
-ln -s /usr/src/linux-stable-5.10.187-lvc10 /usr/src/linux
+tar xf ~/linux-stable-5.10.186-lvc9.tar.xz
+ln -s /usr/src/linux-stable-5.10.186-lvc9 /usr/src/linux
 cd linux
 gzip -cd /root/patch-5.10.186-rt91.patch.gz | patch -p1
 cp /root/config-5.10.180-rt89-amd64 ./.config
@@ -24,12 +24,12 @@ make -j8 modules_install
 make -j8 install
 nice make -j8 bindeb-pkg
 
-update-initramfs -c -k 5.10.180-rt89
+update-initramfs -c -k 5.10.186-rt91
 
-ln -s /boot/initrd.img-5.10.187-rt91 /boot/initrd.img
-ln -s /boot/config-5.10.187-rt91 /boot/config
-ln -s /boot/System.map-5.10.187-rt91 /boot/System.map
-ln -s /boot/vmlinuz-5.10.187-rt91 /boot/vmlinuz
+ln -s /boot/initrd.img-5.10.186-rt91 /boot/initrd.img
+ln -s /boot/config-5.10.186-rt91 /boot/config
+ln -s /boot/System.map-5.10.186-rt91 /boot/System.map
+ln -s /boot/vmlinuz-5.10.186-rt91 /boot/vmlinuz
 
 update-grub
 
