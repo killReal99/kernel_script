1b6224046a961c3d411f2a457b7fac4b648feb70
kirill <kirilllatyshov99@gmail.com>
2024-01-22 13:35:59 +0300

    Update kernel version

--- kernel_script.sh
+++ kernel_script.sh
@@ -9,27 +9,27 @@ kconfig-hardened-check -c /root/config-5.10.186-rt91 -l /proc/cmdline -m show_fa
 
 git config --global http.sslVerify "false"
 
-wget --no-check-certificate  https://git.linuxtesting.ru/pub/scm/linux/kernel/git/lvc/linux-stable.git/snapshot/linux-stable-5.10.186-lvc9.tar.xz
-wget https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.10/patch-5.10.186-rt91.patch.gz
+wget --no-check-certificate  https://git.linuxtesting.ru/pub/scm/linux/kernel/git/lvc/linux-stable.git/snapshot/linux-stable-5.10.204-lvc20.tar.xz
+wget https://cdn.kernel.org/pub/linux/kernel/projects/rt/5.10/patch-5.10.204-rt100.patch.gz
 
 cd /usr/src/
-tar xf ~/linux-stable-5.10.186-lvc9.tar.xz
-ln -s /usr/src/linux-stable-5.10.186-lvc9 /usr/src/linux
+tar xf ~/linux-stable-5.10.204-lvc20.tar.xz
+ln -s /usr/src/linux-stable-5.10.204-lvc20 /usr/src/linux
 cd linux
-gzip -cd /root/patch-5.10.186-rt91.patch.gz | patch -p1
-cp /root/config-5.10.180-rt91 ./.config
+gzip -cd /root/patch-5.10.204-rt100.patch.gz | patch -p1
+cp /root/config-5.10.204-rt100 ./.config
 
 make -j8
 make -j8 modules_install
 make -j8 install
 nice make -j8 bindeb-pkg
 
-update-initramfs -c -k 5.10.186-rt91
+update-initramfs -c -k 5.10.204-rt100
 
-ln -s /boot/initrd.img-5.10.186-rt91 /boot/initrd.img
-ln -s /boot/config-5.10.186-rt91 /boot/config
-ln -s /boot/System.map-5.10.186-rt91 /boot/System.map
-ln -s /boot/vmlinuz-5.10.186-rt91 /boot/vmlinuz
+ln -s /boot/initrd.img-5.10.204-rt100 /boot/initrd.img
+ln -s /boot/config-5.10.204-rt100 /boot/config
+ln -s /boot/System.map-5.10.204-rt100 /boot/System.map
+ln -s /boot/vmlinuz-5.10.204-rt100 /boot/vmlinuz
 
 update-grub
 
